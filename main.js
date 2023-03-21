bf6c6a469fb5803570b78f0f8430ef2a5260c251
Litenoo <lukaszcholewka@outlook.com>
2023-03-21 22:05:38 +0100

    Changed password

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let password = "text";
+let password = "Samuel Pepsi i jego koledzy";
 password = password.toUpperCase();
 let letterMarks = ["A","Ą","B","C","Ć","D","E","Ę","F","G","H","I","J","K","L","Ł","M","N","Ń","O","Ó","P","Q","R","S","Ś","T","U","V","W","X","Y","Z","Ż","Ź"];
 let lengthOfPassword = password.length;
