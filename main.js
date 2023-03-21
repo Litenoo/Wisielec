048373fea7bee781fbc9e9d5cb881a2d5b9b7542
Litenoo <lukaszcholewka@outlook.com>
2023-03-21 21:52:53 +0100

    Final version of project

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-let password = "Miodowe lata";
+let password = "text";
 password = password.toUpperCase();
 let letterMarks = ["A","Ą","B","C","Ć","D","E","Ę","F","G","H","I","J","K","L","Ł","M","N","Ń","O","Ó","P","Q","R","S","Ś","T","U","V","W","X","Y","Z","Ż","Ź"];
 let lengthOfPassword = password.length;
 let hidenPassword = "";
 let numberOfWrongs = 0;
+let yes = new Audio("yes.wav");
+let no = new Audio("no.wav");
 
 for(i = 0; i < lengthOfPassword; i++){
     if(password.charAt(i) == " ") hidenPassword = hidenPassword + " ";
@@ -25,11 +27,10 @@ function generateAlphabet(){
     writePassword();
 }
 
-String.prototype.setMark = function(place, mark)
-{
+String.prototype.setMark = function(place, mark){
 	if (place > this.length - 1) return this.toString();
 	else return this.substr(0, place) + mark + this.substr(place+1);
-}
+};
 
 function checkLetter(id) {
     let element = "ltr" + id;
@@ -41,19 +42,35 @@ function checkLetter(id) {
         }
     }
     if(guessed == true){
+        yes.play();
         let complete = document.getElementById(element).style;
         complete.background = "#003300";
         complete.border = "2px solid #006600"
         complete.color = "green"
         complete.boxShadow = "none";
-        writePassword();  
+        complete.cursor = "default";
+        writePassword();
     }else{
+        no.play();
         let wrong = document.getElementById(element).style;
         wrong.background = "#330000";
         wrong.border = "2px solid #660000"
         wrong.color = "#bb2211"
+        wrong.cursor = "default";
         wrong.boxShadow = "none";
+        document.getElementById(element).setAttribute("onclick",";");
+
         numberOfWrongs++;
         document.querySelector("#gallows").innerHTML = '<img src="images/s' + numberOfWrongs + '.jpg">';
+    }
+    if(password == hidenPassword){
+        document.querySelector("#alphabet").innerHTML = '<h2>Wygrałeś</h2> <br> Gratulacje, odgadnąłeś hasło ! <br><br> <span id="again" onclick="location.reload()"> Kliknij, aby zagrać jeszcze raz ! </span>';
+        document.querySelector("#alphabet").style.display = "block";
+        document.querySelector("#alphabet").style.textAlign = "center";
+    }
+    if(numberOfWrongs >= 9){
+        document.querySelector("#alphabet").innerHTML = '<h2>Przegrałeś</h2> <br> Niestety nie udało ci się odgadnąć hasła. <br><br> <span id="retry" onclick="location.reload()"> Kliknij, aby spróbowąć jeszcze raz. </span>';
+        document.querySelector("#alphabet").style.display = "block";
+        document.querySelector("#alphabet").style.textAlign = "center";
     }
 }
