a1c3443a3c5c0b2ac2f73512a6fb9b272cc052fb
Litenoo <lukaszcholewka@outlook.com>
2023-03-21 15:12:11 +0100

    Wersja robocza

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 let password = "Ciekawe hasło";
+let letterMarks = new Array(35);
+letterMarks = ["A"]
+
 password = password.toUpperCase();
 let lenghtOfPassword = password.length;
-let hidePassword = "";
+let hidenPassword = "";
 
 for(i = 0; i < lenghtOfPassword; i++){
-    if(password.charAt(i) == " ") hidePassword = hidePassword + " ";
-    else hidePassword = hidePassword + "-";
+    if(password.charAt(i) == " ") hidenPassword = hidenPassword + " ";
+    else hidenPassword = hidenPassword + "-";
 }
 
 function writePassword(){
-    let board = document.querySelector("#board").innerHTML = hidePassword;
+    let board = document.querySelector("#board").innerHTML = hidenPassword;
 }
 
 window.onload = generateAlphabet;
 function generateAlphabet(){
     let divContent = "";
     for(i = 0; i <= 34; i++){
-        divContent = divContent + "<div class=\"letter\">A<div>"
+        divContent = divContent + '<div class="letter">A</div>'
+        // <div class="letter"><div> + <div class="letter"><div> = <div class="letter"><div><div class="letter"><div>
     }
-    document.querySelector("#alphabet").innerHTML = divContent;
+    document.querySelector("#letters").innerHTML = divContent;
     writePassword();
 }
